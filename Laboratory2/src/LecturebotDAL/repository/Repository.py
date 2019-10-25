0e384144ffb86095061c10a9772735e5bc1a90a2
AntonAce <sonara4mus@gmail.com>
2019-10-25 20:59:37 +0000

    Create repository tools

---
+++ Laboratory2/src/LecturebotDAL/repository/Repository.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+
+
+class Repository:
+    def __init__(self, session, model_base, db_engine):
+        self.DBEngine = db_engine
+        self.Session = session
+        self.ModelBase = model_base
+
+    def __map_entity(self, entity_model, entity):
+        mapped_values = {}
+
+        for item in entity_model.__dict__.items():
+            field_name = item[0]
+            field_type = item[1]
+            is_column = isinstance(field_type, InstrumentedAttribute)
+            if is_column:
+                mapped_values[field_name] = getattr(entity, field_name)
+
+        return mapped_values
+
+    def get_all(self, entity_model):
+        return self.Session.query(entity_model).all()
+
+    def get_by_id(self, entity_model, identity):
+        return self.Session.query(entity_model).filter_by(id=identity).first()
+
+    def create(self, entity):
+        self.Session.add(entity)
+
+    def update(self, entity_model, entity, identity):
+        self.Session.query(entity_model).filter_by(id=identity).update(self.__map_entity(entity_model, entity))
+
+    def drop(self, entity_model, identity):
+        self.Session.query(entity_model).filter_by(id=identity).delete()
