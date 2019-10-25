d5c8c187bfae2c5c5cb4d8a5af406725fba2c1bc
AntonAce <sonara4mus@gmail.com>
2019-10-25 22:33:26 +0000

    Add Role page mockup

--- Laboratory2/src/LecturebotAPI/app.py
+++ Laboratory2/src/LecturebotAPI/app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+from LecturebotDAL.repository import Repository
+from LecturebotDAL.models import Role, Resource
+
+from LecturebotDAL.dbcontext import *
+
 app = Flask(__name__)
 app.secret_key = 'development key'
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('navigation.html')
+
+
+@app.route('/role', methods=['GET'])
+def list_roles():
+    repository = Repository.Repository(session, ModelBase, DBEngine)
+    roles = repository.get_all(Role.Role)
+
+    return render_template('role.html', roles=roles)
 
