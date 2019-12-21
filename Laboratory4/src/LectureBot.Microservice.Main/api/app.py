12a548618f2e1bf1b325d5c8f1a361c5753f280c
AntonAce <sonara4mus@gmail.com>
2019-12-21 22:39:15 -0100

    Change web site style

--- Laboratory4/src/LectureBot.Microservice.Main/api/app.py
+++ Laboratory4/src/LectureBot.Microservice.Main/api/app.py
@@ -19,11 +19,11 @@ def index():
         res = make_response("")
         res.set_cookie('user', form.Login.data, max_age=None)
         res.set_cookie('token', form.Password.data, max_age=None)
-        res.headers['location'] = url_for('navigation')
+        res.headers['location'] = url_for('page')
         return res, 302
 
     if 'token' in request.cookies:
-        return render_template('navigation.html', user=request.cookies['user'])
+        return render_template('page.html', user=request.cookies['user'])
     else:
         return render_template('login.html', form=form)
 
@@ -37,55 +37,9 @@ def logout():
     return res, 302
 
 
-@app.route('/navigation', methods=['GET', 'POST'])
-def navigation():
-    return render_template('navigation.html', user=request.cookies['user'])
-
-
-@app.route('/role', methods=['GET', 'POST'])
-def list_roles():
-    repository = Repository.Repository(session, ModelBase, DBEngine)
-    unit_of_work = UnitOfWork.UnitOfWork(session, ModelBase)
-    roles = repository.get_all(Role.Role)
-    form = RoleForm.RoleForm(request.form)
-
-    if request.method == 'POST':
-        new_role = Role.Role(name=form.Name.data, priority=form.Priority.data)
-        repository.create(new_role)
-        unit_of_work.commit()
-        return redirect('/role')
-
-    return render_template('role.html', roles=roles, form=form, user=request.cookies['user'])
-
-
-@app.route('/role/delete/<identity>', methods=['GET'])
-def delete_role(identity):
-    repository = Repository.Repository(session, ModelBase, DBEngine)
-    unit_of_work = UnitOfWork.UnitOfWork(session, ModelBase)
-    repository.drop(Role.Role, identity, True)
-    unit_of_work.commit()
-    return redirect('/role')
-
-
-@app.route('/role/edit/<identity>', methods=['GET'])
-def edit_role(identity):
-    form = RoleEditForm.RoleEditForm()
-    form.id.data = identity
-    return render_template('roleedit.html', identity=identity, form=form, user=request.cookies['user'])
-
-
-@app.route('/roleedit', methods=['POST'])
-def save_changes_role():
-    repository = Repository.Repository(session, ModelBase, DBEngine)
-    unit_of_work = UnitOfWork.UnitOfWork(session, ModelBase)
-    form = RoleEditForm.RoleEditForm(request.form)
-
-    new_role = Role.Role(name=form.Name.data, priority=form.Priority.data)
-    new_role.Id = form.id.data
-
-    repository.update(Role.Role, new_role, form.id.data, True)
-    unit_of_work.commit()
-    return redirect('/role')
+@app.route('/page', methods=['GET'])
+def page():
+    return render_template('page.html', user=request.cookies['user'])
 
 
 @app.route('/user', methods=['GET'])
@@ -140,89 +94,3 @@ def save_changes_lecture():
     repository.update(Lecture.Lecture, new_lecture, form.id.data, True)
     unit_of_work.commit()
     return redirect('/lecture')
-
-
-@app.route('/userhasresources', methods=['GET'])
-def list_resources_of_user():
-    repository = Repository.Repository(session, ModelBase, DBEngine)
-    resources_of_user = repository.get_all(UserHasResources.UserHasResources)
-
-    return render_template('userhasresource.html', usersresources=resources_of_user, user=request.cookies['user'])
-
-
-@app.route('/resource', methods=['GET', 'POST'])
-def list_resources():
-    repository = Repository.Repository(session, ModelBase, DBEngine)
-    unit_of_work = UnitOfWork.UnitOfWork(session, ModelBase)
-    resources = repository.get_all(Resource.Resource)
-    form = ResourceForm.ResourceForm(request.form)
-
-    if request.method == 'POST':
-        new_resource = Resource.Resource(url=form.URL.data, description=form.Description.data)
-        repository.create(new_resource)
-        unit_of_work.commit()
-        return redirect('/resource')
-
-    return render_template('resource.html', resources=resources, form=form, user=request.cookies['user'])
-
-
-@app.route('/resource/delete/(<url>)', methods=['GET'])
-def delete_resource(url):
-    repository = Repository.Repository(session, ModelBase, DBEngine)
-    unit_of_work = UnitOfWork.UnitOfWork(session, ModelBase)
-    repository.drop(Resource.Resource, url, False)
-    unit_of_work.commit()
-    return redirect('/lecture')
-
-
-@app.route('/resource/edit/(<url>)', methods=['GET'])
-def edit_resource(url):
-    form = LectureEditForm.LectureEditForm()
-    form.id.data = url
-    return render_template('lectureedit.html', identity=url, form=form, user=request.cookies['user'])
-
-
-@app.route('/resourceedit', methods=['POST'])
-def save_changes_resource():
-    repository = Repository.Repository(session, ModelBase, DBEngine)
-    unit_of_work = UnitOfWork.UnitOfWork(session, ModelBase)
-    form = ResourceEditForm.ResourceEditForm(request.form)
-
-    new_resource = Resource.Resource(url=form.id.data, description=form.Description.data)
-
-    repository.update(Resource.Resource, new_resource, form.id.data, False)
-    unit_of_work.commit()
-    return redirect('/resource')
-
-
-@app.route('/component', methods=['GET'])
-def list_components():
-    repository = Repository.Repository(session, ModelBase, DBEngine)
-    components = repository.get_all(Component.Component)
-
-    return render_template('component.html', components=components, user=request.cookies['user'])
-
-
-@app.route('/attribute', methods=['GET'])
-def list_attributes():
-    repository = Repository.Repository(session, ModelBase, DBEngine)
-    attributes = repository.get_all(Attribute.Attribute)
-
-    return render_template('attribute.html', attributes=attributes, user=request.cookies['user'])
-
-
-@app.route('/dashboard', methods=['GET'])
-def dashboard():
-    repository = Repository.Repository(session, ModelBase, DBEngine)
-    resources = repository.get_all(Resource.Resource)
-
-    repository = ServiceLocator.ServiceLocator(session, ModelBase, DBEngine)
-    res_count = repository.get_count_of_resources_of_user().fetchall()
-
-    urls = [str(resource.URL) for resource in resources]
-    times = [int(resource.TimesVisited) for resource in resources]
-
-    res = [str(resC[0]) for resC in res_count]
-    count = [int(resC[1]) for resC in res_count]
-
-    return render_template('dashboard.html', x1=urls, y1=times, x2=res, y2=count)
