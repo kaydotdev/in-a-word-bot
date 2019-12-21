dda0fd37a3ccaf42dfc0e00a2f2c76d2327846fa
AntonAce <sonara4mus@gmail.com>
2019-12-21 20:29:22 -0100

    Launch settings for main microservice

--- Laboratory4/src/LectureBot.Microservice.Main/api/main.py
+++ Laboratory4/src/LectureBot.Microservice.Main/api/main.py
@@ -1,5 +1,6 @@
 from api.app import app
+from api.settings.ports import MAIN_MICROSERVICE_PORT
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(port=MAIN_MICROSERVICE_PORT)
---
+++ Laboratory4/src/LectureBot.Microservice.Main/api/settings/urls.py
@@ -0,0 +1,1 @@
+AI_SERVICE_URL = "http://127.0.0.1:5001/"
