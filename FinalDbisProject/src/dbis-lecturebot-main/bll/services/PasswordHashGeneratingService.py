7627ea5c78dc1c1a8e3cfe03222c3003830821c8
AntonAce <sonara4mus@gmail.com>
2020-01-06 01:16:13 -0100

    BLL Password Hash Generating Service

---
+++ FinalDbisProject/src/dbis-lecturebot-main/bll/services/PasswordHashGeneratingService.py
@@ -0,0 +1,12 @@
+import hmac
+import hashlib
+
+
+class PasswordHashGeneratingService:
+    def __init__(self, secret):
+        self.secret = secret
+
+    def get_password_hash(self, password):
+        return hmac.new(bytes(self.secret, 'UTF-8'),
+                        msg=bytes(password, 'UTF-8'),
+                        digestmod=hashlib.sha256).hexdigest().upper()
