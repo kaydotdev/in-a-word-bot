cd92cf157dd8485ebfd5c6156b1db05f66399e06
AntonAce <sonara4mus@gmail.com>
2020-01-07 15:11:04 -0100

    Linting names is password generating service

--- FinalDbisProject/src/dbis-lecturebot-main/bll/services/PasswordHashGeneratingService.py
+++ FinalDbisProject/src/dbis-lecturebot-main/bll/services/PasswordHashGeneratingService.py
@@ -6,7 +6,7 @@ class PasswordHashGeneratingService:
     def __init__(self, secret):
         self.secret = secret
 
-    def get_password_hash(self, password):
+    def generate_password_hash(self, password):
         return hmac.new(bytes(self.secret, 'UTF-8'),
                         msg=bytes(password, 'UTF-8'),
                         digestmod=hashlib.sha256).hexdigest().upper()
