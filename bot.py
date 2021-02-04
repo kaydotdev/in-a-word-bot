6c7a81fe4a7fbde5d415c0db88fb603a33745763
Anton Kozyriev <sonara4mus@gmail.com>
2021-02-05 01:49:53 +0200

    feat(bot): add contact links

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ParseMode
 from aiogram.utils.emoji import emojize
 from aiogram.utils.markdown import bold, text, link
-from settings import API_TOKEN
+from settings import *
 from sources import KNOWN_SOURCES
 
 
@@ -15,27 +15,28 @@ dispatcher = Dispatcher(bot)
 
 
 DISCLAIMER = emojize(text(*[
-    ":mag:", "Greetings, I am", bold("'A GRAM OF WORD'"), "bot",
+    ":mag:", "Greetings, I am", bold("'A GRAM OF WORD'"), "bot,",
     "an open Telegram bot that", bold("collects and aggregates"),
     "scientific lectures and articles from the known sources",
     "using meta-searching algorithms and text analysis algorithms.",
     "The result of the aggregation process is a brief text summary",
     "of collected resources.", "Full list of the available commands:\n\n",
     "/list - print known resource list\n",
-    "/query - collect a resource summary by user's topic"
+    "/query - collect a resource summary by user's topic\n\n",
+    link("Source code", REPO_LINK), link("Developer", DEV_LINK)
 ], sep=' '))
 
 
 @dispatcher.message_handler(commands=['start'])
 async def handle_start(message: types.Message):
-    await message.answer(DISCLAIMER, parse_mode=ParseMode.MARKDOWN)
+    await message.answer(DISCLAIMER, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
 
 @dispatcher.message_handler(commands=['list'])
 async def handle_list(message: types.Message):
     sources_list = text(*[text(link(source['name'], source['url']), "-", source['description'], sep=' ')
                           for source in KNOWN_SOURCES], sep='\n')
-    await message.answer(sources_list, parse_mode=ParseMode.MARKDOWN)
+    await message.answer(sources_list, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -2,3 +2,5 @@ import os
 
 
 API_TOKEN = os.getenv('ENV_API_TOKEN')
+DEV_LINK = os.getenv('ENV_DEV_LINK')
+REPO_LINK = os.getenv('ENV_REPO_LINK')
