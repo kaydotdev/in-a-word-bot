cc14dedc0463fc3e99b366bead090a96388c0b1c
Anton Kozyriev <sonara4mus@gmail.com>
2021-02-06 18:51:37 +0200

    feat(processing.extractors): citizendium site indexer implementation

--- settings.py
+++ settings.py
@@ -2,5 +2,6 @@ import os
 
 
 API_TOKEN = os.getenv('ENV_API_TOKEN')
+MAX_SOURCE_POOL = int(os.getenv('ENV_MAX_SOURCE_POOL'))
 DEV_LINK = os.getenv('ENV_DEV_LINK')
 REPO_LINK = os.getenv('ENV_REPO_LINK')
