37cda187d063c90ae1136405b5d48b89da575c2e
Anton Kozyriev <sonara4mus@gmail.com>
2021-02-08 21:48:28 +0200

    feat(processing.indexers): oxforde indexer implementation

--- processing/indexers.py
+++ processing/indexers.py
@@ -4,7 +4,7 @@ from lxml import html
 
 class CitizendiumIndexer:
     domain = 'https://en.citizendium.org'
-    link_weight = 0.05
+    link_weight = 0.3
 
     def __query_scheme__(self, topic) -> str:
         query_topic = topic.replace(' ', '+')
@@ -29,7 +29,7 @@ class CitizendiumIndexer:
 
 class EveripediaIndexer:
     domain = 'https://everipedia.org'
-    link_weight = 0.2
+    link_weight = 0.3
 
     async def index(self, topic) -> list:
         request_body = {
@@ -49,5 +49,30 @@ class EveripediaIndexer:
                     'href': 'https://everipedia.org/wiki/lang_en/' + article['slug'],
                     'weight': self.link_weight
                 } for article in articles]
+
+        return links
+
+
+class OxfordreIndexer:
+    domain = 'https://oxfordre.com'
+    link_weight = 0.4
+
+    def __query_scheme__(self, topic) -> str:
+        query_topic = topic.replace(' ', '+')
+        return f"{self.domain}/search?q={query_topic}&searchBtn=Search&isQuickSearch=true"
+
+    def __extract__(self, document: html.HtmlElement, weight: float) -> list:
+        search_results = document.cssselect('#searchContent div.contentItem.hasAccess')
+        return [{
+            'title': result.cssselect('div.title-wrapper a')[0].text_content(),
+            'href': self.domain + result.cssselect('div.title-wrapper a')[0].attrib['href'],
+            'weight': weight
+        } for result in search_results]
+
+    async def index(self, topic) -> list:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.__query_scheme__(topic)) as response:
+                document = html.fromstring(await response.text())
+                links = self.__extract__(document, self.link_weight)
 
         return links
