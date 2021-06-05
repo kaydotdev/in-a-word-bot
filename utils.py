780e13ea4492bde59d1c76f3981b48ab599eded3
Anton Kozyriev <sonara4mus@gmail.com>
2021-06-05 15:43:59 +0300

    feat(utils): clear html from meta-tags

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import re
-
+from lxml import html
 
 re_clean_html_tags = re.compile(r"<.*?>")
 re_match_http_url = re.compile(r"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$")
@@ -7,4 +7,13 @@ re_file_format = re.compile(r"^.*\.(txt|TXT)$")
 
 
 def remove_html_tags(response: str):
-    return re.sub(re_clean_html_tags, '', response)
+    def __remove_all_by_xpath__(__tree__, __xpath__):
+        for el in __tree__.xpath(__xpath__):
+            el.getparent().remove(el)
+
+    tree = html.fromstring(response)
+
+    for xpath in ["//head", "//script", "//style"]:
+        __remove_all_by_xpath__(tree, xpath)
+
+    return re.sub(re_clean_html_tags, '', html.tostring(tree).decode('utf-8'))
