53bcaece917e5ed2b36f76364afe7ef5936ea296
Anton Kozyriev <sonara4mus@gmail.com>
2021-09-21 00:15:17 +0300

    feat: store text for processing in azure tables

--- src/endpoint/apiendpoint/processing.py
+++ src/endpoint/apiendpoint/processing.py
@@ -1,5 +1,9 @@
 import base64
+
 from json import dumps
+from uuid import uuid4
+
+from azure.data.tables.aio import TableServiceClient
 
 from azure.storage.queue.aio import QueueClient
 from azure.storage.queue import BinaryBase64EncodePolicy
@@ -17,13 +21,18 @@ blob_service_client = BlobServiceClient.from_connection_string(
     conn_str=REQUEST_STORAGE_CONNECTION_STRING
 )
 
+table_service_client = TableServiceClient.from_connection_string(
+    conn_str=REQUEST_STORAGE_CONNECTION_STRING
+)
+
+table_client = table_service_client.get_table_client(table_name="requeststates")
+
 
 async def send_to_processing_queue(chat_id: int, text: str, criteria: str):
-    request = {
-        "chat_id": chat_id,
-        "text": text,
-        "criteria": criteria,
-    }
+    request_headers = { "PartitionKey": str(uuid4()), "RowKey": str(chat_id) }
+    request = { **request_headers, 'Text': text, 'Type': criteria, 'State': 'PENDING' }
+
+    await table_client.create_entity(entity=request)
 
     message = dumps(request).encode('ascii')
     await queue.send_message(base64.b64encode(message))
